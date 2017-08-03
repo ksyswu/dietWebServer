eccd4fa2fcd0d065b98b840b2f95e35a595cb996
ksyswu <usingname@naver.com>
2017-08-03 14:24:58 +0900

    toadsql
    database

---
+++ SWU_TEAM5/tjoin2017-08-03 14-24-02.sql
@@ -0,0 +1,22 @@
+USE SWU_TEAM5;
+
+CREATE TABLE `tjoin` (
+  `USER_ID` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `USER_PW` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CM` int(11) DEFAULT NULL,
+  `KG` int(11) DEFAULT NULL,
+  `H_KG` int(11) DEFAULT NULL,
+  `WORD` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`USER_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+insert into `tjoin`(`USER_ID`,`USER_PW`,`CM`,`KG`,`H_KG`,`WORD`) values ('111','111',111,80,50,'111');
+insert into `tjoin`(`USER_ID`,`USER_PW`,`CM`,`KG`,`H_KG`,`WORD`) values ('1234','1234',160,60,40,'');
+insert into `tjoin`(`USER_ID`,`USER_PW`,`CM`,`KG`,`H_KG`,`WORD`) values ('222','222',160,55,40,'');
+insert into `tjoin`(`USER_ID`,`USER_PW`,`CM`,`KG`,`H_KG`,`WORD`) values ('123','123',0,0,0,'');
+insert into `tjoin`(`USER_ID`,`USER_PW`,`CM`,`KG`,`H_KG`,`WORD`) values ('bbb','bbb',40,40,40,'dsdfsfsdfsdf
+sfsdf');
+insert into `tjoin`(`USER_ID`,`USER_PW`,`CM`,`KG`,`H_KG`,`WORD`) values ('jjjj','',0,0,0,'');
+insert into `tjoin`(`USER_ID`,`USER_PW`,`CM`,`KG`,`H_KG`,`WORD`) values ('sohyun4283','alsdk',155,55,45,'우와된다수정');
+insert into `tjoin`(`USER_ID`,`USER_PW`,`CM`,`KG`,`H_KG`,`WORD`) values ('1111','33',0,0,0,'');
---
+++ SWU_TEAM5/weight2017-08-03 14-24-02.sql
@@ -0,0 +1,24 @@
+USE SWU_TEAM5;
+
+CREATE TABLE `weight` (
+  `DATE` datetime NOT NULL,
+  `D_KG` varchar(20) DEFAULT NULL,
+  `USER_ID` varchar(20) NOT NULL,
+  PRIMARY KEY (`DATE`,`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-07-27 00:00:00','80','111');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-07-28 00:00:00','70','111');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-07-29 00:00:00','75','111');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-07-30 00:00:00','70','111');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-07-30 00:00:00','65','bbbb');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-07-31 00:00:00','50','bbbb');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-08-01 00:00:00','70','111');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-08-01 00:00:00','5','123');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-08-01 00:00:00','10','bbb');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-08-02 00:00:00','47','');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-08-02 00:00:00','50','1234');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-08-02 00:00:00','5000','12345');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-08-02 00:00:00','50','222');
+insert into `weight`(`DATE`,`D_KG`,`USER_ID`) values ('2017-08-03 00:00:00','60','111');
